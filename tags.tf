126d800c672f2a8c9eda0f072044846ba8726c1a
root <kevin.sandermann@gmail.com>
2020-04-27 08:48:35 +0000

    update

--- tags.tf
+++ tags.tf
@@ -2,6 +2,8 @@ locals {
   tags_base = {
     provisioned_automatically = "true"
     provisioned_by            = "terraform"
+    environment               = var.environment
+    organsiation              = var.organisation
   }
 
 }
